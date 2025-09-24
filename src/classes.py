2bc601f6907e578801d28f8973a18b943078026b
Nate Goethel <nategoethel@gmail.com>
2025-09-23 22:23:15 -0500

    start using APIs

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
 class Album:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+    def __init__(self, title, domain, url_override):
+        self.name = title
+        self.domain = domain
+        self.url_override = url_override
